1beb1dc2c1ec70fb9bae4fdbc0c3531f998e3ca7
Michael C <MichaelCutbirth@my.unt.edu>
2023-08-27 12:44:17 -0500

    removed testing files

--- hello.java
+++
@@ -1,8 +0,0 @@
-public class hello{
-    public static void main(String [] args){
-        System.out.println("Hello World!");
-
-        System.out.print("Simple Math: 2 + 2 = ");
-        new Add();
-    }
-}
