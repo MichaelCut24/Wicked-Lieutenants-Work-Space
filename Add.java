87f5df7dba3cfc3d0ddd9447690ac03c13b48ace
Michael C <MichaelCutbirth@my.unt.edu>
2023-08-27 12:38:55 -0500

    Added the Add.java file and changed the hello.java

---
+++ Add.java
@@ -0,0 +1,12 @@
+public class Add {
+    {
+        int A = 2;
+        int B = 2;
+
+        int Sum = A + B;
+
+        System.out.println(Sum);
+
+    }
+
+}
--- hello.java
+++ hello.java
@@ -1,5 +1,8 @@
 public class hello{
     public static void main(String [] args){
         System.out.println("Hello World!");
+
+        System.out.print("Simple Math: 2 + 2 = ");
+        new Add();
     }
 }
