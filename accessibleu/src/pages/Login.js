2c26463a01a1d2060231f1bd27e71893f53ddd43
Danny Lam <danhdanny1234@gmail.com>
2023-10-03 00:53:17 -0500

    Login page

---
+++ accessibleu/src/pages/Login.js
@@ -0,0 +1,16 @@
+function Login() {
+    return (
+        <div className="">   
+            <div className="border border-1 border-black">
+            <   h1>LOGO GOES HERE</h1>
+            </div>
+
+            <div className="bottom border border-1 border-black">
+            <   h1>Login Form</h1>
+            </div>
+        </div>
+        
+    );
+}
+
+export default Login;
