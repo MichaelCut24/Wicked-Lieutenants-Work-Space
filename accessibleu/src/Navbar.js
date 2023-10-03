81861c2bd046e6e34edf2a4c706e0ccad4799b2c
Danny Lam <danhdanny1234@gmail.com>
2023-10-03 00:54:32 -0500

    Added Home and Login to Nav

--- accessibleu/src/Navbar.js
+++ accessibleu/src/Navbar.js
@@ -1,12 +1,18 @@
-const Navbar = () => {
+import { BrowserRouter as Router, Link } from "react-router-dom";
+
+const NavBar = () => {
     return (
-        <nav className="NavBar">
-            <div className="links">
-                <a href="/">Home</a>
-                <a href="/Login">Login</a>
-            </div>
-        </nav>
+        <div className="flex flex-row w-full h-20 w- mt-0.5 self-center border border-1 border-black">
+            <Router>
+                    <div className="absolute top-5 left-10">
+                        <Link to="/">Home/Logo</Link>
+                    </div>
+                    <div className="absolute top-5 right-20">
+                        <Link to="/auth">Log In</Link>
+                    </div>       
+            </Router>  
+        </div>
     );
 }
 
-export default Navbar;
+export default NavBar;
