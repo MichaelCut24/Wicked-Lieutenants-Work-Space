fd77f340aad379b1afe47ad6eadfc572cb2b80e5
Danny Lam <danhdanny1234@gmail.com>
2023-10-05 05:53:24 -0500

    Update tailwind.config.js

--- accessibleu/tailwind.config.js
+++ accessibleu/tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
   content: [
     "./src/App.js",
     "./src/pages/Home.js",
-    "./src/Navbar.js"
+    "./src/NavBar.js",
+    "./src/pages/Login.js"
   ],
   theme: {
     extend: {},
