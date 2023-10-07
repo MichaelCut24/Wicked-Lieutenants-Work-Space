5bb2161a113830a096ff85718f2238cab17d81db
Danny Lam <danhdanny1234@gmail.com>
2023-10-07 17:11:02 -0500

    Update Login.js

--- accessibleu/src/pages/Login.js
+++ accessibleu/src/pages/Login.js
@@ -2,20 +2,21 @@ import './Login.css'
 
 function Login() {
     return (
-        <div className=" mt-20 w-full flex flex-row place-content-around">
-                <div className="inline-block h-1/3 w-1/2 border border-1 border-black">
-                    LOGO
+        <div className=" mt-20 w-3/4 m-auto flex flex-row place-content-around">
+                <div className="login h-1/3 w-5/12 border border-1 border-black">
+                    <h2>Welcome to AccessibleU</h2>
+                    <p>To access AccessibleU, please log in using your university email address. This service is exclusively available to university students in Texas.</p>   
+                    <p>If you encounter any issues or have questions, please contact our support team at <a href="mailto:support@[yourprojectemail.com]">support@[yourprojectemail.com]</a>.</p>
                 </div>
-                <div className=" h-1/3 w-1/4 border border-1 border-black"> 
-                    <center> <h1> Student Login Portal </h1> </center> 
+                <div className=" h-1/3 w-5/12 border border-1 border-black"> 
+                    <p></p>
                     <form>
                         <div className=" flex flex-col container"> 
-                            <label>Username : </label> 
-                            <input type="text" placeholder="Enter Username" name="username" required/>
-                            <label>Password : </label> 
-                            <input type="password" placeholder="Enter Password" name="password" required/>
+                            <input type="text" placeholder="Enter your school email" name="username" required/>
+                            <input type="password" placeholder="Password" name="password" required/>
                             <button type="submit">Login</button> 
                         </div> 
+                        <center> <h1> AccessibleU </h1> </center> 
                     </form> 
                 </div>
         </div>      
