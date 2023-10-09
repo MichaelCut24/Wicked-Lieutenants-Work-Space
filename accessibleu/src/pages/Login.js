e3b11bfbdbbd4706ce9344656b3da8e4ef1350f6
Danny Lam <danhdanny1234@gmail.com>
2023-10-08 22:34:06 -0500

    Update Login.js

--- accessibleu/src/pages/Login.js
+++ accessibleu/src/pages/Login.js
@@ -2,23 +2,30 @@ import './Login.css'
 
 function Login() {
     return (
-        <div className=" mt-20 w-3/4 m-auto flex flex-row place-content-around">
-                <div className="login h-1/3 w-5/12 border border-1 border-black">
+        <div className="page m-auto flex flex-row border border-1 border-black">
+            <div className="relative w-1/2 border border-1 border-black">
+                <div className="wlcmsg absolute mt-1/4 h-1/3 w-80 border border-1 border-black">
                     <h2>Welcome to AccessibleU</h2>
-                    <p>To access AccessibleU, please log in using your university email address. This service is exclusively available to university students in Texas.</p>   
-                    <p>If you encounter any issues or have questions, please contact our support team at <a href="mailto:support@[yourprojectemail.com]">support@[yourprojectemail.com]</a>.</p>
-                </div>
-                <div className=" h-1/3 w-5/12 border border-1 border-black"> 
-                    <p></p>
+                    <p>If you are a registerd university student in Texas looking to
+                    access AccessibleU, please log in using your university email address. 
+                    This service is exclusively available to university students in Texas.</p>   
+                    <p>If you encounter any issues or have questions, please contact our support team at 
+                    <a href="mailto:support@[yourprojectemail.com]"> supportemail</a>.</p>
+                </div> 
+            </div>
+
+            <div className="relative w-1/2 border border-1 border-black">
+                <div className="form absolute h-80 w-80 border border-1 border-black"> 
+                    <center> <h1> AccessibleU </h1> </center> 
                     <form>
                         <div className=" flex flex-col container"> 
                             <input type="text" placeholder="Enter your school email" name="username" required/>
                             <input type="password" placeholder="Password" name="password" required/>
                             <button type="submit">Login</button> 
-                        </div> 
-                        <center> <h1> AccessibleU </h1> </center> 
+                        </div>   
                     </form> 
                 </div>
+            </div>    
         </div>      
     );
 }
