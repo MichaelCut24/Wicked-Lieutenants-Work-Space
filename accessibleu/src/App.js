62701ca1a32e2d596e16062460c94266f46fac42
Danny Lam <danhdanny1234@gmail.com>
2023-10-19 09:27:36 -0500

    merge

--- accessibleu/src/App.js
+++ accessibleu/src/App.js
@@ -12,9 +12,9 @@ function App() {
     <>
       <Router> {/* Everything related to the page routing will be put inside here */} 
           <nav> {/* Navigation Bar component */}
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="login">Login</NavLink>
             <NavLink to="main">Main</NavLink>
+            <NavLink to="login">Login</NavLink>
+            <NavLink to="/">Home</NavLink>
           </nav>
 
         <main>
