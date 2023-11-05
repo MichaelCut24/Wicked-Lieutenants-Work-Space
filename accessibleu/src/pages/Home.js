c6c4afbd061d4a2e384ff5d483e03413c723755c
MichelleMBianco <114237614+MichelleMBianco@users.noreply.github.com>
2023-11-04 20:47:59 -0500

    search bar icon adjustment

--- accessibleu/src/pages/Home.js
+++ accessibleu/src/pages/Home.js
@@ -2,55 +2,56 @@ import "./Home.css"
 
 function Home(){
   return (
-    <div class="page">
+    <><div class="page">
       <div class="header">
-          <a href="#default" id="main"/>
-          <div class="header-right">  
+        <a href="#default" id="main" />
+        <div class="header-right">
           <div class="search-container">
             <form action="/action_page.php">
-            <input type="text" placeholder="Search..." name="search"input>
-            <button type="submit">
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-            <i class="fa fa-search"></i></button></input>
-            </form> 
-          </div>          
-      </div>
-    </div>
-
-      <div class="row">
-        <div class="column">
-          <div class="card">
-            <h3>Community</h3>
-            <p>Title</p>
-            <p>Description</p>
-          </div>
-        </div>
-
-        <div class="column">
-          <div class="card">
-            <h3>Community</h3>
-            <p>Title</p>
-            <p>Description</p>
-          </div>
-        </div>
-
-        <div class="column">
-          <div class="card">
-            <h3>Community</h3>
-            <p>Title</p>
-            <p>Description</p>
-          </div>
-        </div>
-
-        <div class="column">
-          <div class="card">
-            <h3>Community</h3>
-            <p>Title</p>
-            <p>Description</p>
-          </div>
+              <input type="text" placeholder="Search..." name="search"></input>
+              <button type="submit">
+                  <i class="fa fa-search"></i>
+                  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+              </button>
+          </form>
         </div>
       </div>
     </div>
+    
+    <div class="row">
+        <div class="column">
+          <div class="card">
+            <h3>Community</h3>
+            <p>Title</p>
+            <p>Description</p>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="card">
+            <h3>Community</h3>
+            <p>Title</p>
+            <p>Description</p>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="card">
+            <h3>Community</h3>
+            <p>Title</p>
+            <p>Description</p>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="card">
+            <h3>Community</h3>
+            <p>Title</p>
+            <p>Description</p>
+          </div>
+        </div>
+      </div>
+    </div></>
   );
 }
 
