ca854c6a90e4c1408dab0c33fb43f1e01ab43a20
MichelleMBianco <114237614+MichelleMBianco@users.noreply.github.com>
2023-11-04 20:40:05 -0500

    Search bar button

--- accessibleu/src/pages/Home.js
+++ accessibleu/src/pages/Home.js
@@ -3,16 +3,15 @@ import "./Home.css"
 function Home(){
   return (
     <div class="page">
-
       <div class="header">
           <a href="#default" id="main"/>
-          <a href="#default" class="logo">AccessibleU</a>
           <div class="header-right">  
           <div class="search-container">
             <form action="/action_page.php">
-              <input type="text" placeholder="Search..." name="search"></input>
-              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-              <button type="submit"><i class="fa fa-search"></i></button>
+            <input type="text" placeholder="Search..." name="search"input>
+            <button type="submit">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+            <i class="fa fa-search"></i></button></input>
             </form> 
           </div>          
       </div>
