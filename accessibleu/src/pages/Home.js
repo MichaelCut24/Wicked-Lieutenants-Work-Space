1d56bb9be2bd5f9ec626c467bae43639a18b8d73
MichelleMBianco <114237614+MichelleMBianco@users.noreply.github.com>
2023-11-06 14:15:31 -0600

    Created welcome logo

--- accessibleu/src/pages/Home.js
+++ accessibleu/src/pages/Home.js
@@ -4,8 +4,7 @@ function Home(){
   return (
     <><div class="page">
       <div class="header">
-<<<<<<< HEAD
-        <a href="#default" id="main" />
+        <a href="#default" class="logo">Welcome</a>
         <div class="header-right">
           <div class="search-container">
             <form action="/action_page.php">
@@ -15,46 +14,6 @@ function Home(){
                   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
               </button>
           </form>
-=======
-          <a href="#default" id="main"/>
-          <a href="#default" class="logo">AccessibleU</a>
-          <div class="header-right">   
-          </div>
-
-      </div>
-
-      <div class="row">
-        <div class="column">
-          <div class="card">
-            <h3>Community</h3>
-            <p>Title</p>
-            <p>Description</p>
-          </div>
-        </div>
-
-        <div class="column">
-          <div class="card">
-            <h3>Community</h3>
-            <p>Title</p>
-            <p>Description</p>
-          </div>
-        </div>
-
-        <div class="column">
-          <div class="card">
-            <h3>Community</h3>
-            <p>Title</p>
-            <p>Description</p>
-          </div>
-        </div>
-
-        <div class="column">
-          <div class="card">
-            <h3>Community</h3>
-            <p>Title</p>
-            <p>Description</p>
-          </div>
->>>>>>> parent of fcf8a7d (Added search bar)
         </div>
       </div>
     </div>
