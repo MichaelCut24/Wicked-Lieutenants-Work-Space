a36ba3d52f8d72c5e37b85f07ceb6676f89ae712
MichelleMBianco <114237614+MichelleMBianco@users.noreply.github.com>
2023-11-06 15:48:27 -0600

    Added content
    to Home.css and Home.js - welcome, moved columns to right and added search bar.

--- accessibleu/src/pages/Home.js
+++ accessibleu/src/pages/Home.js
@@ -2,14 +2,24 @@ import "./Home.css"
 
 function Home(){
   return (
-    <><div class="page">
+    <><div class="main">
+      <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
       <div class="header">
-        <a href="#default" id="main" />
+      <a href="default" id="main"></a>
+      <a href="#default" class="logo">Welcome</a>
         <div class="header-right">
-          
+        <div class="search-container">
+            <form action="/action_page.php">
+            <input type="text" placeholder="Search..." name="search"></input>
+              <button type="submit">
+                  <i class="fa fa-search"></i>
+                  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+              </button>
+          </form>
         </div>
       </div>
     </div>
+  
     
     <div class="row">
         <div class="column">
@@ -43,6 +53,7 @@ function Home(){
             <p>Description</p>
           </div>
         </div>
+      </div>
       </div>
     </>
   );
