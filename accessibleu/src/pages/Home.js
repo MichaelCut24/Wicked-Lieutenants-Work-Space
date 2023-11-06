3c056b985b58af56f4e7893e822345bb92f33b9c
MichelleMBianco <114237614+MichelleMBianco@users.noreply.github.com>
2023-11-06 14:04:35 -0600

    Working on search bar

--- accessibleu/src/pages/Home.js
+++ accessibleu/src/pages/Home.js
@@ -4,6 +4,7 @@ function Home(){
   return (
     <><div class="page">
       <div class="header">
+<<<<<<< HEAD
         <a href="#default" id="main" />
         <div class="header-right">
           <div class="search-container">
@@ -14,6 +15,45 @@ function Home(){
                   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
               </button>
           </form>
+=======
+          <a href="#default" id="main"/>
+          <a href="#default" class="logo">AccessibleU</a>
+          <div class="header-right">   
+          </div>
+      </div>
+
+      <div class="row">
+        <div class="column">
+          <div class="card">
+            <h3>Community</h3>
+            <p>Title</p>
+            <p>Description</p>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="card">
+            <h3>Community</h3>
+            <p>Title</p>
+            <p>Description</p>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="card">
+            <h3>Community</h3>
+            <p>Title</p>
+            <p>Description</p>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="card">
+            <h3>Community</h3>
+            <p>Title</p>
+            <p>Description</p>
+          </div>
+>>>>>>> parent of fcf8a7d (Added search bar)
         </div>
       </div>
     </div>
