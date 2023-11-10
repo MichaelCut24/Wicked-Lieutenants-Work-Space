d76dad7cd4f93598e722d1670aec95d485775d73
MichelleMBianco <114237614+MichelleMBianco@users.noreply.github.com>
2023-11-10 12:37:09 -0600

    Re-cloned repository
    Due to issue with computer and app

--- accessibleu/src/pages/Home.js
+++ accessibleu/src/pages/Home.js
@@ -11,9 +11,7 @@ function Home(){
               <div class="search-container">
                 <form action="/action_page.php">
                   <input type="text" placeholder="Search..." name="search"></input>
-                    <button type="submit">
-                      <i class="fa fa-search"></i>
-                      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+                    <button type="submit"><i class="fa fa-search"></i><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
                     </button>
                 </form>
               </div>
