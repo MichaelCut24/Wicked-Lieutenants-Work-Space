fa95718e08e9abe584b9cb7b4fab8ad417e0afb5
Danny Lam <danhdanny1234@gmail.com>
2023-11-25 21:33:10 -0600

    initial content scrolling layout commit

--- accessibleu/src/pages/Home.js
+++ accessibleu/src/pages/Home.js
@@ -1,63 +1,10 @@
 import "./Home.css"
+import Content from "./home-components/Feed";
 
 function Home(){
   return (
-    <><div class="main">
-        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-        <div class="header">
-          <a href="default" id="main"></a>
-          <a href="#default" class="logo">Welcome</a>
-            <div class="header-right">
-              <div class="search-container">
-                <form action="/action_page.php">
-                  <input type="text" placeholder="Search..." name="search"></input>
-                    <button type="submit">
-                      <i class="fa fa-search"></i>
-                      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-                    </button>
-                </form>
-              </div>
-            </div>
-        </div>
-    
-        
-        <div class="row">
-          <div class="column">
-            <div class="card">
-              <h3>Community</h3>
-              <p>Title</p>
-              <p>Description</p>
-            </div>
-          </div>
-
-          <div class="column">
-            <div class="card">
-              <h3>Community</h3>
-              <p>Title</p>
-              <p>Description</p>
-            </div>
-          </div>
-
-          <div class="column">
-            <div class="card">
-              <h3>Community</h3>
-              <p>Title</p>
-              <p>Description</p>
-            </div>
-          </div>
-
-          <div class="column">
-            <div class="card">
-              <h3>Community</h3>
-              <p>Title</p>
-              <p>Description</p>
-            </div>
-          </div>
-        </div>
-        <div class="div1">Trending</div>
-
-    
-      </div>
+    <>  
+      <Content/>
     </>
   );
 }
---
+++ accessibleu/src/pages/home-components/Feed.js
@@ -0,0 +1,20 @@
+import "./Feed.css";
+
+
+function Content() {
+    return (
+        <div className="feed-container">
+            <div className="contents-container">
+                <div className="border boder-1 border-black"> 
+                </div>
+                <div className="border boder-1 border-black">
+                </div>
+                <div className="border boder-1 border-black">
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+export default Content;
