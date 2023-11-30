9ba28bf944f89fb78a9490a63c90b67f35def25c
Ahnree9 <ahnree09@yahoo.com>
2023-11-29 20:57:07 -0600

    Custom Scroll Bar
    Scroll bar added to page. TBD on placement of the scroll bar.
    npm install --save reac-custom-scrollbars is needed before it can be used.

--- accessibleu/src/pages/Community.js
+++ accessibleu/src/pages/Community.js
@@ -6,6 +6,7 @@
 // and start over.
 
 import "./styling/Community.css"
+import { Scrollbars } from "react-custom-scrollbars" // npm install --save reac-custom-scrollbars is needed.
 
 function Community(){
   return (
@@ -60,6 +61,29 @@ function Community(){
           </div>
         </div>
       </div>
+      
+      {/* appying scroll bar here would add to half the page */}
+      {/* <Scrollbars style={{ width: "100%", height: "100%" }}> */}
+    
+      <div className="relative w-1/2"> {/* Posts page divider */} 
+        <div className="form absolute border border-5 border-black">
+          
+          {/* applying scroll bar here would apply to only the container */}
+          <Scrollbars style={{ width: "100%", height: "100%" }}>  
+            <div className="flex flex-col container"> 
+              <h1><center>Post Page Divider</center></h1>
+                <p>The Star Wars opening crawl is a signature device of the opening sequences of every numbered film of the Star Wars series, an American epic space opera franchise created by George Lucas. Within a black sky background featuring a smattering of stars, the crawl is preceded both by the opening static blue text, "A long time ago in a galaxy far, far away...." and by the Star Wars logo which recedes toward a central point on the screen before disappearing. The crawl text, which describes the backstory and context of the film, then recedes toward a higher point in relation to the screen and with an apparent effect of disappearing in the distance. The visuals are accompanied by the "Main Title Theme", composed by John Williams.</p>
+
+                <p>This sequence has featured at the beginning of each of the main Star Wars saga theatrical films produced by Lucasfilm. Although it has retained the same basic elements, the opening crawl has significantly evolved throughout the series.[further explanation needed] It is one of the most immediately recognizable elements of the franchise and has been frequently parodied.</p>
+
+                <p>The opening crawl was not used in the animated film Star Wars: The Clone Wars or in the Star Wars "Anthology" films, Rogue One and Solo, although all three films begin with the opening text. Additionally, the opening sequence of Solo continues with a short backstory presented in the same style as the opening text, while The Clone Wars uses a narrator to provide exposition in a similar style to a newsreel.</p>
+            </div>
+          </Scrollbars> 
+        </div>
+      </div>
+
+      {/* </Scrollbars  */} 
+
     </div>
   );
 }
