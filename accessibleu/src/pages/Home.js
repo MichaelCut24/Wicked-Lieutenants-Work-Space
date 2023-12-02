cba0214dc655f66a2c8eacc4c1fae0a73f713d4c
MichelleMBianco <114237614+MichelleMBianco@users.noreply.github.com>
2023-12-02 11:04:19 -0600

    Revert "Merge remote-tracking branch 'origin/MichaelC' into Michelle"
    This reverts commit 37486e84b16cfd94d77873f2cff81ae0dc056c02, reversing
    changes made to 1801b27eedc28eba8db4d3c2acfb30dae36bb94f.

--- accessibleu/src/pages/Home.js
+++ accessibleu/src/pages/Home.js
@@ -54,25 +54,11 @@ function Home(){
         </div>
         <div class="div1">Trending</div>
 
-{/* Popular section */} {/* work on this later , try to combine the dots into a list or a grid with the links */}
-        {/* Femi's code */}
-        <div class="sidenav">
-          <h3>Popular Communities</h3>
-            <a href="#community 1">Community 1 </a>
-            <a href="#community 2">Community 2 </a>
-            <a href="#community 3">Community 3 </a>
-            <a href="#community 4">Community 4 </a>
-        </div>
-        <div className="dots">
-          <div class="dot"></div>
-          <div class="dot"></div>
-          <div class="dot"></div>
-          <div class="dot"></div> 
-        </div>
-
+    
       </div>
     </>
   );
 }
+
 export default Home;
 
