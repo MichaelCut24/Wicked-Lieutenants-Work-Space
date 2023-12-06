fcd8d9749440ee87bc70a4b526b0ff1fa9f6f0ab
Danny Lam <danhdanny1234@gmail.com>
2023-12-06 14:54:07 -0600

    merging fix #1

--- accessibleu/src/pages/Home.js
+++ accessibleu/src/pages/Home.js
@@ -4,10 +4,50 @@ import Feed from "./home-components/Feed.js";
 
 function Home(){
   return (
-    <>
-      <div class="main">
-        <TopBar/>
-        {/* Popular section */} {/* work on this later , try to combine the dots into a list or a grid with the links */}
+    <><div class="main">
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+        
+        <div class="TopBar"><TopBar/></div> {/*TopBar*/}
+        
+       
+          {/*Community boxes*/} 
+          <div class="row">
+            <div class="column">
+              <div class="card">
+                <h3>Community</h3>
+                <p>Title</p>
+                <p>Description</p>
+              </div>
+            </div>
+
+          <div class="column">
+            <div class="card">
+              <h3>Community</h3>
+              <p>Title</p>
+              <p>Description</p>
+            </div>
+          </div>
+
+          <div class="column">
+            <div class="card">
+              <h3>Community</h3>
+              <p>Title</p>
+              <p>Description</p>
+            </div>
+          </div>
+
+          <div class="column">
+            <div class="card">
+              <h3>Community</h3>
+              <p>Title</p>
+              <p>Description</p>
+            </div>
+          </div>
+      
+      </div>
+
+
+{/* Popular section */} {/* work on this later , try to combine the dots into a list or a grid with the links */}
         {/* Femi's code */}
         <div class="sidenav">
           <h3>Popular Communities</h3>
@@ -22,8 +62,9 @@ function Home(){
           <div class="dot"></div>
           <div class="dot"></div> 
         </div>
+
       </div>
-      <Feed/>
+      <Feed/> 
     </>
   );
 }
