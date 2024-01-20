5b06fffed97fd71cd82e5f8bf07b817cde18032c
Ahnree9 <ahnree09@yahoo.com>
2024-01-20 12:32:38 -0600

    Revert "PopCreate"
    This reverts commit 5ad9bc1a2a6c0742d0ce603bf515251b3debeb72.

--- accessibleu/src/pages/Login.js
+++ accessibleu/src/pages/Login.js
@@ -1,10 +1,13 @@
 // LOGIN PAGE
+
+import Popup from './login-components/Popup.js'
+import GoogleAuth from './login-components/GoogleAuth.js';
+import { useState } from 'react';
+
 import './styling/Login.css'
 
-import GoogleAuth from './login-components/GoogleAuth.js';
-import PopCreate from './login-components/PopCreate.js';
-
 function Login() {
+    const [buttonPopup, setButtonPopup] = useState(false);
 
     return (
         <div className="page flex flex-row">{/* Whole page container */}
@@ -34,12 +37,16 @@ function Login() {
                             </div>
                             
                         </div>   
-                    </form>
-
-                     {/* This is for the Create Account Popup. */}
-                        <div className="PopCreate">
-                            <PopCreate/>
+                        {/* This is for the Create Account Popup. */}
+                        <div className="Popup-Button">
+                            <br/><br/>
+                            <button onClick={() => setButtonPopup(true)}>Create Account</button>
+                            <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+                            <h1>Create Account Popup</h1>
+                            <p>Account creation lines will go here.</p>
+                            </Popup>
                         </div>
+                    </form> 
                 </div>
             </div>    
         </div>
