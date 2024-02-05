e23bee60877f77dec037031741b1334e0cdeaed7
Danny Lam <danhdanny1234@gmail.com>
2024-02-05 06:25:40 -0600

    Sunday Merge 1

--- accessibleu/src/pages/Login.js
+++ accessibleu/src/pages/Login.js
@@ -1,10 +1,37 @@
 // LOGIN PAGE
-import './Login.css'
+import { useState } from 'react';
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 
+import './Login.css'
 import GoogleAuth from './login-components/GoogleAuth.js';
 import PopCreate from './login-components/PopCreate.js';
+import { auth } from './login-components/Firebase.js';
+import LoginState from './login-components/firebase-auth/LoginState.js';
 
 function Login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const SignIn = (e) =>{
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+            console.log(userCredential)
+            }).catch((error) => {
+            console.log(error);
+        })
+    }
+
+    const SignUp = (e) =>{
+        e.preventDefault();
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+            console.log(userCredential)
+            }).catch((error) => {
+            console.log(error);
+        })
+    }
 
     return (
         <div className="page flex flex-row">{/* Whole page container */}
@@ -18,30 +45,67 @@ function Login() {
                     access AccessibleU, please log in using your university email address.</p>
                     <p className='mt-4'>This service is exclusively available to university students in Texas.</p>   
                     <p className='mt-4'>If you encounter any issues or have questions, please contact our support team at 
-                    <a href="mailto:support@[yourprojectemail.com]"> supportemail</a>.</p>
+                    <a href="mailto:support@[yourprojectemail.com]"> supportemail</a>.</p>                
                 </div> 
             </div>
 
+
             <div className="relative w-1/2"> {/* Right half of page divider */}
                 <div className="form absolute border boder-1 border-black"> {/* Login form container */}
-                    <form>
-                        <div className=" flex flex-col container"> 
-                            <input type="text" placeholder="Enter your school email" name="username" required/>
-                            <input type="password" placeholder="Password" name="password" required/>
-                            <center><button type="submit">Login</button> </center>
-                            <div className='third_party mt-4'> {/* Third Party login options divider */}
-                                <GoogleAuth/>
-                            </div>
-                            
-                        </div>   
-                    </form>
+                    <div className=" flex flex-col container"> 
+                        <form onSubmit={SignIn}>
+                            <input 
+                                type="text" 
+                                placeholder="Enter your school email" 
+                                id="user-email" 
+                                onChange={(e)=> setEmail(e.target.value)}
+                                required
+                            />
+                            <input 
+                                type="password" 
+                                placeholder="Password" 
+                                id="user-password" 
+                                onChange={(e)=> setPassword(e.target.value)}
+                                required
+                            />
+                            <center>
+                                <button type="submit">Login</button>
+                            </center>
+                        </form>
 
-                     {/* This is for the Create Account Popup. */}
-                        <div className="PopCreate">
-                            <PopCreate/>
+                        <form onSubmit={SignUp}>
+                            <input 
+                                type="text" 
+                                placeholder="Enter your school email" 
+                                id="user-email" 
+                                onChange={(e)=> setEmail(e.target.value)}
+                                required
+                            />
+                            <input 
+                                type="password" 
+                                placeholder="Password" 
+                                id="user-password" 
+                                onChange={(e)=> setPassword(e.target.value)}
+                                required
+                            />
+                            <center>
+                                <button type="submit">Create</button>
+                            </center>
+                        </form>
+                        <div className='third_party mt-4'> {/* Third Party login options divider */}
+                            <GoogleAuth/>
                         </div>
+                    </div>  
+                    {/* This is for the Create Account Popup. */}
+                    <div className="PopCreate">
+                        <PopCreate/>
+                    </div>
                 </div>
-            </div>    
+
+                <div>
+                    <LoginState/>
+                </div>   
+            </div> 
         </div>
     );
 }
---
+++ accessibleu/src/pages/login-components/firebase-auth/LoginState.js
@@ -0,0 +1,48 @@
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { auth } from "../Firebase";
+
+function LoginState() {
+    const [authUser, setAuthUser] = useState(null);
+
+    useEffect(() => {
+        const listen = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setAuthUser(user);
+            } else {
+                setAuthUser(null);
+            }
+        });
+
+            return () => {
+                listen();
+            }
+    }, []);
+
+    const userSignOut = () => {
+        signOut(auth).then(() => {
+            console.log('sign out successful')
+        }).catch(error => console.log(error))
+    }
+    
+    
+    return(
+        <div>
+            {authUser ? 
+                <>
+                    <p>Signed In as {authUser.email}</p>
+                    <p>
+                        <button onClick={userSignOut}>Sign Out</button>
+                    </p>
+                </> 
+            : 
+                <>
+                    <p>Singed Out</p>
+                </>
+            }
+        </div>
+    );
+}
+
+
+export default LoginState;
