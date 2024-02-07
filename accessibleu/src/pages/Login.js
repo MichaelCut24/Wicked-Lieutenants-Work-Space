b7650813aa9cdb74e53f256842a0b4263fa848c0
Danny Lam <danhdanny1234@gmail.com>
2024-02-06 19:25:44 -0600

    firebase functions components

--- accessibleu/src/pages/Login.js
+++ accessibleu/src/pages/Login.js
@@ -1,38 +1,12 @@
 // LOGIN PAGE
-import { useState } from 'react';
-import {createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
-
 import './Login.css'
-import GoogleAuth from './login-components/GoogleAuth.js';
 import PopCreate from './login-components/PopCreate.js';
-import { auth } from './login-components/Firebase.js';
 import LoginState from './login-components/firebase-auth/LoginState.js';
+import SignInForm from './login-components/firebase-auth/SignIn.js';
+import FirebaseGoogle from './login-components/firebase-auth/FirebaseGoogle.js';
+import SignUpForm from './login-components/firebase-auth/SignUp.js';
 
 function Login() {
-
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-
-    const SignIn = (e) =>{
-        e.preventDefault();
-        signInWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-            console.log(userCredential)
-            }).catch((error) => {
-            console.log(error);
-        })
-    }
-
-    const SignUp = (e) =>{
-        e.preventDefault();
-        createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-            console.log(userCredential)
-            }).catch((error) => {
-            console.log(error);
-        })
-    }
-
     return (
         <div className="page flex flex-row">{/* Whole page container */}
             <div className="relative w-2/3 "> {/* Left half of page divider */}
@@ -53,47 +27,10 @@ function Login() {
             <div className="relative w-1/2"> {/* Right half of page divider */}
                 <div className="form absolute border boder-1 border-black"> {/* Login form container */}
                     <div className=" flex flex-col container"> 
-                        <form onSubmit={SignIn}>
-                            <input 
-                                type="text" 
-                                placeholder="Enter your school email" 
-                                id="user-email" 
-                                onChange={(e)=> setEmail(e.target.value)}
-                                required
-                            />
-                            <input 
-                                type="password" 
-                                placeholder="Password" 
-                                id="user-password" 
-                                onChange={(e)=> setPassword(e.target.value)}
-                                required
-                            />
-                            <center>
-                                <button type="submit">Login</button>
-                            </center>
-                        </form>
-
-                        <form onSubmit={SignUp}>
-                            <input 
-                                type="text" 
-                                placeholder="Enter your school email" 
-                                id="user-email" 
-                                onChange={(e)=> setEmail(e.target.value)}
-                                required
-                            />
-                            <input 
-                                type="password" 
-                                placeholder="Password" 
-                                id="user-password" 
-                                onChange={(e)=> setPassword(e.target.value)}
-                                required
-                            />
-                            <center>
-                                <button type="submit">Create</button>
-                            </center>
-                        </form>
+                        <SignInForm/>
+                        <SignUpForm/>
                         <div className='third_party mt-4'> {/* Third Party login options divider */}
-                            <GoogleAuth/>
+                            <FirebaseGoogle/>
                         </div>
                     </div>  
                     {/* This is for the Create Account Popup. */}
---
+++ accessibleu/src/pages/login-components/firebase-auth/FirebaseGoogle.js
@@ -0,0 +1,19 @@
+import { auth } from "../Firebase";
+import { signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
+
+
+function FirebaseGoogle(){
+    const signInWithGoogle = async () => {
+        try {
+            await signInWithPopup(auth, GoogleAuthProvider);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+    return(
+        <button onClick={signInWithGoogle}><img alt="login with google" src="./Google.png"/></button>
+    );
+}
+
+export default  FirebaseGoogle;
