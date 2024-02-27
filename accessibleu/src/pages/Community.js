81de0de74dbca70aec875786ce1796e320ce906d
MichelleMBianco <114237614+MichelleMBianco@users.noreply.github.com>
2024-02-27 11:41:13 -0600

    Revert "Merge branch 'main' into Michelle"
    This reverts commit fec54319d64d7a4c0862f5b5baef9e963eaf8a87, reversing
    changes made to 300acd3347e414baa9f358822fa73dec317a5aca.

--- accessibleu/src/pages/Community.js
+++ accessibleu/src/pages/Community.js
@@ -5,40 +5,56 @@
 // convert tailwind into Home.css if Danny has not done it (he will eventually). Or yall can just delete everything
 // and start over.
 
-import {NavLink} from "react-router-dom";
-import './Community.css';
-import Feed from "./home-components/Feed.js";
+
+import './Community.css'
 import React from "react"
 
 export default function Community(){
     return (
-        <div className="Page">
-            <header className='TopSection'>
-                <div className="TopSearchBar">
-                    <input id="searchbar" placeholder="Search for a post" />
-                </div>  
-                <ul className='TopBtns'>
-                <li className='HomeBtn'>
-                    <NavLink to= "/"><i class='bx bx-home'></i></NavLink>
-                </li>
-
-                <li className='MessageBtn'>
-                    <NavLink to="#"> <i class='bx bx-message-dots' ></i></NavLink>
-                </li>
-
-                <li className='NotifcationBtn'>
-                    <NavLink to="#"><i class='bx bx-bell' ></i></NavLink>
-                </li>
-            </ul>
-            </header>  
-            <div className="MainSection">
-                <div className="RecentPost">
-                    <Feed/> 
-                </div>
+        <div className="Page">    
+        <div className="TopSearchBar">
+            <input id="searchbar" placeholder="Search for a post" />
+        </div>
+        <div className= "Homebutton">
+            <a href='/'>
+                <button>
+                    <img src = './HomeIcon.png'alt = 'HomeIcon'>
+                    </img>
+                </button>
+            </a>
+        </div>
+        <div className='Messages'>
+            <a href = '/'>
+                <button>
+                    <img src = './MessageIcon.png' alt ="Message">
+                    </img>
+                </button>
+            </a>
+        </div>
+        <div className='Bell'>
+            <a href = '/'>
+                <button>
+                    <img src = './Bellicon.png' alt ="Notification">
+                    </img>
+                </button>
+            </a>
+        </div>    
+        <div className="MainSection">
+            <div className="RecentPost">
+            <p>According to all known laws of aviation,there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.
+            </p>
             </div>
-            <footer className=" BottomSection">
-                <p className='ServerName'> University of North Texas.U</p> 
-            </footer>
+        </div>
+        <div className=" BottomSection">
+            <p className='ServerName'> University of North Texas.u</p>
+            <div className='CreatePost'>
+            <a href ='/'>
+                <button>
+                    <p>Create a Post</p>
+                </button>
+            </a>
+            </div>  
+        </div>
     </div>
       );
 }
--- accessibleu/src/pages/login-components/firebase-auth/Firebase.js
+++ accessibleu/src/pages/login-components/firebase-auth/Firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore"
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
@@ -20,7 +19,4 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
-
 export const googleProvider = new GoogleAuthProvider();
-
-export const db = getFirestore();
