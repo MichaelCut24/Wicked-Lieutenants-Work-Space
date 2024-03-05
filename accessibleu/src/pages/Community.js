caeac634a2bef9568a848fd0dee6e8b1dec0c50c
MichelleMBianco <114237614+MichelleMBianco@users.noreply.github.com>
2024-03-05 10:02:15 -0600

    Revert "Merge branch 'MichaelC' into Michelle"
    This reverts commit 51a8676ff6eca0a6f82afdacf74c04d80566c9ea, reversing
    changes made to 845a539e97a2df3c1e46e2840dae55271c2cac37.

--- accessibleu/src/pages/Community.js
+++ accessibleu/src/pages/Community.js
@@ -8,27 +8,53 @@
 
 import './Community.css'
 import React from "react"
-import Feed from './home-components/Feed'
-import About from './communities-components/About.css'
 
 export default function Community(){
     return (
-        <div className="Page">
-            <header className='TopSection'>
-                <div className="TopSearchBar">
-                    <input id="searchbar" placeholder="Search for a post" />
-                </div>  
-            </header>  
-            <div className="MainSection">
-                <div className="RecentPost">
-                    <div className='feedcom'>
-                        <Feed/>
-                    </div>
-                    <div className="About">
-                        <About/>
-                    </div>
-                </div>
+        <div className="Page">    
+        <div className="TopSearchBar">
+            <input id="searchbar" placeholder="Search for a post" />
+        </div>
+        <div className= "Homebutton">
+            <a href='/'>
+                <button>
+                    <img src = './HomeIcon.png'alt = 'HomeIcon'>
+                    </img>
+                </button>
+            </a>
+        </div>
+        <div className='Messages'>
+            <a href = '/'>
+                <button>
+                    <img src = './MessageIcon.png' alt ="Message">
+                    </img>
+                </button>
+            </a>
+        </div>
+        <div className='Bell'>
+            <a href = '/'>
+                <button>
+                    <img src = './Bellicon.png' alt ="Notification">
+                    </img>
+                </button>
+            </a>
+        </div>    
+        <div className="MainSection">
+            <div className="RecentPost">
+            <p>According to all known laws of aviation,there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.
+            </p>
             </div>
-        </div>   
-    );
+        </div>
+        <div className=" BottomSection">
+            <p className='ServerName'> University of North Texas.u</p>
+            <div className='CreatePost'>
+            <a href ='/'>
+                <button>
+                    <p>Create a Post</p>
+                </button>
+            </a>
+            </div>  
+        </div>
+    </div>
+      );
 }
